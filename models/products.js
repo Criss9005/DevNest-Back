c9e82b519d72af0f27f155ab9dd0dea13c47a8ce
nicolas0745 <133726349+nicolas0745@users.noreply.github.com>
2024-05-15 17:07:43 -0500

    Empiezo a agregar endpoint para productos diarios

--- models/products.js
+++ models/products.js
@@ -1,18 +1,26 @@
-const products = require('./product-schema')
+const products = require("./product-schema");
 
 const paginationOptions = {
   page: 1,
   limit: 20,
-}
-
+};
 
 const listProducts = async () => {
   //const result = await Contact.find({owner: ownerId})
-  const result = await products.paginate({}, paginationOptions)
-  return result
-}
+  const result = await products.paginate({}, paginationOptions);
+  return result;
+};
+
+const addConsumedProduct = async (data) => {
+  const { productName, idUser, date } = data;
+  const product = {
+    productName,
+    idUser,
+    // date,
+  };
+};
 
 module.exports = {
   listProducts,
-  
-}
+  addConsumedProduct,
+};
--- routes/api/products.js
+++ routes/api/products.js
@@ -1,14 +1,19 @@
-const express = require('express')
-const {listProducts } = require('../../models/products')
+const express = require("express");
+const { listProducts, addProductsOfTheDay } = require("../../models/products");
 const productsRouter = express.Router();
 
-    
+productsRouter.get("/", async (req, res, next) => {
+  const users = await listProducts();
+  res.status(200).send(users);
+});
 
-productsRouter.get('/', async (req, res, next) => {
-  
-      const users = await listProducts()
-      res.status(200).send(users)
-      
-})
+productsRouter.post("/products/day"),
+  async (req, res) => {
+    const { foods, dateOfRegister } = req.body;
+    const infoOfTheDay = {
+      foods,
+      dateOfRegister,
+    };
+  };
 
-module.exports = productsRouter
+module.exports = productsRouter;
