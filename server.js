8a4bf26002cfa227b1d0029a69b1d63dc3a0dddf
Criss9005 <cdjpn5@gmail.com>
2024-05-19 12:49:38 -0500

    blacklist rdy

--- server.js
+++ server.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-const express = require("express");
-const connectDB = require("./config/db");
-const authRoutes = require("./routes/api/authRoutes");
-const productRoutes = require("./routes/api/productRoutes");
-require("dotenv").config();
-
-const app = express();
-
-// Connect Database
-connectDB();
-
-// Init Middleware
-app.use(express.json({ extended: false }));
-
-// Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-=======
 const mongoose = require("mongoose");
 const app = require("./app");
 
@@ -35,4 +12,3 @@ app.listen(process.env.PORT, async () => {
     });
   console.log(`Server running. Use our API on port: ${process.env.PORT}`);
 });
->>>>>>> Dev
