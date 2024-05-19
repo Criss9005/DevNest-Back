33a545d1b2a0c9b1869fd2e235ebc93fb0f2cd3c
Michael Caceres <michaeling10@hotmail.com>
2024-05-18 23:43:48 -0500

    Swagger Michael Implementation

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/api/authRoutes");
 const productRoutes = require("./routes/api/productRoutes");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 require("dotenv").config();
 
 const app = express();
@@ -11,6 +13,47 @@ connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
+
+// Swagger configuration
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Calories API",
+      version: "1.0.0",
+      description: "API Documentation for the App",
+      contact: {
+        name: "API Support",
+        url: "http://www.myapp.com",
+        email: "support@myapp.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:5000",
+        description: "Local server",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./routes/api/*.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Routes
 app.use("/api/auth", authRoutes);
