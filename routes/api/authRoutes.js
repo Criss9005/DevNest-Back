c90f4e082a02d5f54c3ae1c268afdb3ddf139bac
Cristian Perez <133727809+Criss9005@users.noreply.github.com>
2024-05-19 13:27:31 -0500

    Revert "Michael -  Tasks 5,6,7 Backend"

--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -3,70 +3,7 @@ const { register, login } = require("../../controllers/authController");
 
 const router = express.Router();
 
-/**
- * @swagger
- * tags:
- *   name: Auth
- *   description: Authentication related endpoints
- */
-
-/**
- * @swagger
- * /api/auth/register:
- *   post:
- *     summary: Register a new user
- *     tags: [Auth]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               name:
- *                 type: string
- *               email:
- *                 type: string
- *               password:
- *                 type: string
- *             example:
- *               name: Mike
- *               email: mike@example.com
- *               password: password123
- *     responses:
- *       200:
- *         description: Successfully registered
- *       400:
- *         description: Bad request
- */
 router.post("/register", register);
-
-/**
- * @swagger
- * /api/auth/login:
- *   post:
- *     summary: Login a user
- *     tags: [Auth]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               email:
- *                 type: string
- *               password:
- *                 type: string
- *             example:
- *               email: mike@example.com
- *               password: password123
- *     responses:
- *       200:
- *         description: Successfully logged in
- *       400:
- *         description: Bad request
- */
 router.post("/login", login);
 
 module.exports = router;
